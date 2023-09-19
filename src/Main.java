126d2bdaff330b8f94914e10be37ff1b0b847b60
Clara <kimdayoung771@gmail.com>
2023-09-19 21:47:13 +0900

    class, getter, setter

--- src/Main.java
+++ src/Main.java
@@ -2,31 +2,13 @@ package src;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println(getDurationString(-3945));
-        System.out.println(getDurationString(-65, 45));
-        System.out.println(getDurationString(65, 145));
-        System.out.println(getDurationString(65, 45));
-        System.out.println(getDurationString(3945));
-    }
+        Car car = new Car();
 
-    public static String getDurationString(int seconds) {
-        if(seconds < 0) {
-            return "";
-        }
-        int minutes = seconds / 60;
-        return getDurationString(minutes, seconds % 60);
-    }
-
-    public static String getDurationString(int minutes, int seconds) {
-        if(minutes < 0) {
-            return "";
-        }
-        if(seconds <=0 || seconds >= 60) {
-            return "";
-        }
-        int hours = minutes / 60;
-        int remainingMinutes = minutes % 60;
-        return hours + ":" + remainingMinutes + ":" + seconds;
-
+        System.out.println(car.isConvertible());
+        car.setConvertible(false);
+        System.out.println(car.isConvertible());
+        car.setMake("Holden");
+        System.out.println(car.getMake());
+        car.describeCar();
     }
 }
