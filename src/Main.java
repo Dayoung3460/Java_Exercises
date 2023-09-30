f8f67dbf845618febd418d26f53843e0ccbdb9d5
Clara <kimdayoung771@gmail.com>
2023-09-30 14:48:58 +0900

    POJO, Record

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,36 @@
-package src;
-
 public class Main {
     public static void main(String[] args) {
-//        Account bobsAccount = new Account("1234", 1000.00, "bob", "bob@bob.com", "01000000000");
-        src.Account bobsAccount = new src.Account();
-        System.out.println(bobsAccount.getNumber());
-//        bobsAccount.setNumber("1234");
-//        bobsAccount.setBalance(1000.00);
-//        bobsAccount.setCustomerName("bob");
-//        bobsAccount.setCustomerPhone("01000000000");
-//        bobsAccount.setGetCustomerEmail("bob@bob.com");
-//        bobsAccount.depositFunds(250);
-//        bobsAccount.withdrawFunds(350);
+        for(int i = 1; i <= 5; i++) {
+            LPAStudent s = new LPAStudent("s92300" + i,
+                    switch (i) {
+                case 1 ->"Mary";
+                case 2 -> "Carol";
+                case 3 -> "Tim";
+                case 4 -> "Harry";
+                case 5 -> "Lisa";
+                        default -> "Anonymous";
+                    },
+                    "05/11/1985",
+                    "Java Masterclass"
+                    );
+            System.out.println(s);
+        }
+        Student pojoStudent = new Student("is9230006", "Ann", "05/11/1995", "Java Masterclass");
+        LPAStudent recordStudent = new LPAStudent("s923007", "Bill", "05/11/1985", "Java Masterclass");
+        Student pojoStudent2 = new Student("is9230006", "Ann", "05/11/1995", "Java Masterclass");
+        System.out.println(pojoStudent);
+        System.out.println(recordStudent);
+
+        pojoStudent.setClassList(pojoStudent.getClassList() + '2');
+//        recordStudent.setClassList(pojoStudent.getClassList());
+
+
+
+        System.out.println(pojoStudent.getName());
+        System.out.println(pojoStudent.getClassList());
+        System.out.println(pojoStudent2.getClassList());
+        System.out.println(recordStudent.name());
+        System.out.println(recordStudent.classList());
     }
 
 }
