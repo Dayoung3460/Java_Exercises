a061d0a4f197e9d2a5b73a859c30336f136a73cb
Clara <kimdayoung771@gmail.com>
2023-09-30 16:56:25 +0900

    inheritance

--- src/Main.java
+++ src/Main.java
@@ -1,36 +1,27 @@
 public class Main {
     public static void main(String[] args) {
-        for(int i = 1; i <= 5; i++) {
-            LPAStudent s = new LPAStudent("s92300" + i,
-                    switch (i) {
-                case 1 ->"Mary";
-                case 2 -> "Carol";
-                case 3 -> "Tim";
-                case 4 -> "Harry";
-                case 5 -> "Lisa";
-                        default -> "Anonymous";
-                    },
-                    "05/11/1985",
-                    "Java Masterclass"
-                    );
-            System.out.println(s);
-        }
-        Student pojoStudent = new Student("is9230006", "Ann", "05/11/1995", "Java Masterclass");
-        LPAStudent recordStudent = new LPAStudent("s923007", "Bill", "05/11/1985", "Java Masterclass");
-        Student pojoStudent2 = new Student("is9230006", "Ann", "05/11/1995", "Java Masterclass");
-        System.out.println(pojoStudent);
-        System.out.println(recordStudent);
+        Animal animal = new Animal ("Generic Animal", "Huge", 400.0);
+        doAnimalStuff(animal, "slow");
 
-        pojoStudent.setClassList(pojoStudent.getClassList() + '2');
-//        recordStudent.setClassList(pojoStudent.getClassList());
+        Dog dog = new Dog();
+        doAnimalStuff(dog, "fast");
 
+        Dog yorkie = new Dog("Yorkie", 15);
+        doAnimalStuff(yorkie, "fast");
 
+        Dog retriever = new Dog("Retriever", 65, "Floppy", "Swimmer");
+        doAnimalStuff(retriever, "slow");
 
-        System.out.println(pojoStudent.getName());
-        System.out.println(pojoStudent.getClassList());
-        System.out.println(pojoStudent2.getClassList());
-        System.out.println(recordStudent.name());
-        System.out.println(recordStudent.classList());
+        Dog wolf = new Dog("Wolf", 40);
+        doAnimalStuff(wolf, "slow");
+
+        Fish goldie = new Fish("Goldfish", 0.25, 2, 3);
+        doAnimalStuff(goldie, "fast");
     }
-
+    public static void doAnimalStuff(Animal animal, String speed) {
+        animal.makeNoise();
+        animal.move(speed);
+        System.out.println(animal);
+        System.out.println("-----------------------------");
+    }
 }
