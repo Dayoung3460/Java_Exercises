1bbba44a122f8c7aef22f76540e26eb891c12dbe
Clara <kimdayoung771@gmail.com>
2023-11-10 21:12:35 +0900

    ArrayList

---
+++ src/arrayLists/dev/lpa/Main.java
@@ -0,0 +1,47 @@
+package arrayLists.dev.lpa;
+
+// record
+//불변(immutable) 데이터 객체를 쉽게 생성할 수 있도록 하는 새로운 유형의 클래스
+// 컴파일러는 파라미터를 보고 내부 필드 추론
+// 생성자 작성하지 않아도 됨
+//final 클래스임(상속불가)
+// abstract 선언 불가
+// 다른 클래스 상속 받을 수도 없음
+//
+
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+record GroceryItem(String name, String type, int count) {
+    public GroceryItem(String name) {
+        this(name, "DAIRY", 1);
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%d %s in %s", count, name.toUpperCase(), type);
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        Object[] groceryArray = new Object[3];
+        groceryArray[0] = new GroceryItem("milk");
+        groceryArray[1] = new GroceryItem("apples", "PRODUCE", 6);
+        groceryArray[2] = new GroceryItem("oranges", "PRODUCE", 5);
+        System.out.println(Arrays.toString(groceryArray));
+
+        ArrayList objectList = new ArrayList();
+        objectList.add(new GroceryItem("Butter"));
+        objectList.add("Yogurt");
+
+        ArrayList<GroceryItem> groceryList = new ArrayList<>();
+        groceryList.add(new GroceryItem("Butter"));
+        groceryList.add(new GroceryItem("milk"));
+        groceryList.add(new GroceryItem("oranges", "PRODUCE", 5));
+        groceryList.set(0, new GroceryItem("apples", "PRODUCE", 6));
+        groceryList.remove(1);
+        System.out.println(groceryList);
+    }
+}
