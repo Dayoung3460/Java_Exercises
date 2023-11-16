1db654d6ae4be6e24c5e0bf96c90c29fc6a1d42f
Clara <kimdayoung771@gmail.com>
2023-11-16 19:18:18 +0900

    comment modified

--- src/arrayLists/dev/lpa/Main.java
+++ src/arrayLists/dev/lpa/Main.java
@@ -3,7 +3,8 @@ package arrayLists.dev.lpa;
 // record
 //불변(immutable) 데이터 객체를 쉽게 생성할 수 있도록 하는 새로운 유형의 클래스
 // 컴파일러는 파라미터를 보고 내부 필드 추론
-// 생성자 작성하지 않아도 됨
+// 생성자, getter, hashCode(), equals() ,toString()를 기본적으로 제공해줌
+// 필요하면 오버라이딩해서 재정의 가능
 //final 클래스임(상속불가)
 // abstract 선언 불가
 // 다른 클래스 상속 받을 수도 없음
