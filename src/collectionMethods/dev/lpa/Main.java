5d2849fabbb7076149f670b5e001f9d8fb1e2f23
Clara <kimdayoung771@gmail.com>
2024-02-06 21:50:42 +0900

    java.util.Collections

---
+++ src/collectionMethods/dev/lpa/Main.java
@@ -0,0 +1,29 @@
+package collectionMethods.dev.lpa;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        List<Card> deck = Card.getStandardDeck();
+        Card.printDeck(deck);
+
+        Card[] cardArray = new Card[13];
+        Card aceOfHearts = Card.getFaceCard(Card.Suit.HEART, 'A');
+        Arrays.fill(cardArray, aceOfHearts);
+        Card.printDeck(Arrays.asList(cardArray), "Aces of Hearts", 1);
+
+        List<Card> cards = new ArrayList<>(52);
+        Collections.fill(cards, aceOfHearts);
+        System.out.println(aceOfHearts);
+        System.out.println(cards);
+        System.out.println("cards.size() = " + cards.size());
+
+        List<Card> acesOfHearts = Collections.nCopies(13, aceOfHearts);
+        Card.printDeck(acesOfHearts, "Aces of Hearts", 1);
+
+
+    }
+}
