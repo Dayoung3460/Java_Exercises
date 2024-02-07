e9d5761290cd5d865c68ccb8b84bf5e2505b1663
Clara <kimdayoung771@gmail.com>
2024-02-07 21:49:12 +0900

    java.util.Collections (shuffle, reverse sort)

--- src/collectionMethods/dev/lpa/Main.java
+++ src/collectionMethods/dev/lpa/Main.java
@@ -1,29 +1,58 @@
 package collectionMethods.dev.lpa;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
-        List<Card> deck = Card.getStandardDeck();
-        Card.printDeck(deck);
+
 
         Card[] cardArray = new Card[13];
         Card aceOfHearts = Card.getFaceCard(Card.Suit.HEART, 'A');
         Arrays.fill(cardArray, aceOfHearts);
         Card.printDeck(Arrays.asList(cardArray), "Aces of Hearts", 1);
 
+        // 용량을 52로 만든다는거지 어떤 요소로 채워져 있지 않음. null로도 채워져 있지 않음
         List<Card> cards = new ArrayList<>(52);
+        // fill 메서드는 요소를 추가하지 않음. cards가 0이기 때문에 채워지지 않음. 기본값을 넣어줘야함.
         Collections.fill(cards, aceOfHearts);
         System.out.println(aceOfHearts);
-        System.out.println(cards);
-        System.out.println("cards.size() = " + cards.size());
+        System.out.println(cards); // []
+        System.out.println("cards.size() = " + cards.size());  // 0
+
 
         List<Card> acesOfHearts = Collections.nCopies(13, aceOfHearts);
         Card.printDeck(acesOfHearts, "Aces of Hearts", 1);
 
+        Card kingOfClubs = Card.getFaceCard(Card.Suit.CLUB, 'K');
+        List<Card> kingsOfClubs = Collections.nCopies(13, kingOfClubs);
+        Card.printDeck(kingsOfClubs, "kingOfClubs", 1);
+
+        Collections.addAll(cards, cardArray);
+        Collections.addAll(cards, cardArray);
+        Card.printDeck(cards, "Card Collection with Aces added", 2);
+
+
+        // cards의 크기가 kingsOfClubs의 크기보다 작으면 카피안됨
+        // cards에 원래 들어있던 값은 없어짐
+        Collections.copy(cards, kingsOfClubs);
+        Card.printDeck(cards, "Card Collection with Kings copied", 2);
+
+        cards = List.copyOf(kingsOfClubs);
+        Card.printDeck(cards, "List Copy of Kings", 1);
+
+        List<Card> deck = Card.getStandardDeck();
+        Card.printDeck(deck);
+
+        Collections.shuffle(deck);
+        Card.printDeck(deck, "Shuffled Deck", 4);
+
+        Collections.reverse(deck);
+        Card.printDeck(deck, "Reversed Deck of Cards", 4);
+
+        var sortingAlgorithm = Comparator.comparing(Card::rank)
+                .thenComparing(Card::suit);
+        Collections.sort(deck, sortingAlgorithm);
+        Card.printDeck(deck, "Standard Deck sorted by rank, suit", 13);
 
     }
 }
