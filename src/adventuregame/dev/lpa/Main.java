1edc9520f898f37b35b51c0fa098ecb42e7a91f9
Clara <kimdayoung771@gmail.com>
2024-02-26 14:50:12 +0900

    adventure game done

--- src/adventuregame/dev/lpa/Main.java
+++ src/adventuregame/dev/lpa/Main.java
@@ -4,8 +4,13 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        AdventureGame game = new AdventureGame();
-        game.play("road");
+        String myLocations = """        
+                lake,at the edge of Lake Tim,E:ocean,W:forest,S:well house,N:cave
+                ocean,on Tim's beach before an angry sea,W:lake
+                cave,at the mouth of Tim's bat cave,E:ocean,W:forest,S:lake
+                """;
+        AdventureGame game = new AdventureGame(myLocations);
+        game.play("lake");
 
         Scanner scanner = new Scanner(System.in);
         while (true) {
