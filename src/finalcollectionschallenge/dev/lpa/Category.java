dc71f41457ad5bacdad11bfdfae03538f6dfe737
Clara <kimdayoung771@gmail.com>
2024-03-04 22:33:49 +0900

    🚧: Collections final challenge

---
+++ src/finalcollectionschallenge/dev/lpa/Category.java
@@ -0,0 +1,5 @@
+package finalcollectionschallenge.dev.lpa;
+
+public enum Category {
+    PRODUCE, DAIRY, CEREAL, MEAT, BEVERAGE
+}
---
+++ src/finalcollectionschallenge/dev/lpa/InventoryItem.java
@@ -0,0 +1,61 @@
+package finalcollectionschallenge.dev.lpa;
+
+public class InventoryItem {
+    private Product product;
+    private double price;
+    private int qtyTotal;
+    private int qtyReserved;
+    private int qtyReorder;
+    private int qtyLow;
+
+    public InventoryItem(Product product, double price, int qtyTotal, int qtyLow) {
+        this.product = product;
+        this.price = price;
+        this.qtyTotal = qtyTotal;
+        this.qtyLow = qtyLow;
+        this.qtyReorder = qtyTotal;
+    }
+
+    public Product getProduct() {
+        return product;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public boolean reserveItem(int qty) {
+        if ((qtyTotal - qtyReserved) >= qty) {
+//          카트에 들어있던 아이템 갯수 + qty
+            qtyReserved += qty;
+            return true;
+        }
+        return false;
+    }
+
+    public void releaseItem(int qty) {
+        // 카트에 들어있던 아이템 갯수 중에서 qty만큼 뺌
+        qtyReserved -= qty;
+    }
+
+    public boolean sellItem(int qty) {
+        if (qtyTotal >= qty) {
+            qtyTotal -= qty;
+            qtyReserved -= qty;
+            if (qtyTotal <= qtyLow) {
+                placeInventoryOrder();
+            }
+            return true;
+        }
+        return false;
+    }
+
+    private void placeInventoryOrder() {
+        System.out.printf("Ordering qty %d : %s%n", qtyReorder, product);
+    }
+
+    @Override
+    public String toString() {
+        return "%s, $%.2f : [%04d,% 2d]".formatted(product, price, qtyTotal, qtyReserved);
+    }
+}
