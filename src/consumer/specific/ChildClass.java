7516c90d51081d5656915b96da0b5e971434e2fe
Clara <kimdayoung771@gmail.com>
2024-03-13 21:49:41 +0900

    hiding vs. overriding

--- src/consumer/specific/ChildClass.java
+++ src/consumer/specific/ChildClass.java
@@ -5,6 +5,8 @@ import finalexplored.dev.lpa.generic.BaseClass;
 // cmd + n -> override 목록을 봤을 때 부모의 정적(static) 메소드는 없음
 // final 이거나 private 메소드도 없음
 public class ChildClass extends BaseClass {
+    //  부모와 자식의 정적 메소드 이름(public static void recommendedStatic)이 같을 때 자식거 무시하고 부모거 호출됨(hiding)
+    // 인스턴스 메소드이면(static 아님) overriding 함
     public static void recommendedStatic() {
         System.out.println("[ChildClass.recommendedStatic] BEST Way to Do it");
         optionalStatic();
