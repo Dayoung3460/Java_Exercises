fe8927560a6dc514fe7ef87b6b7e6837623bf04c
Clara <kimdayoung771@gmail.com>
2024-03-14 21:47:21 +0900

    The final modifier applied to methods, and local variables

--- src/finalexplored/dev/lpa/Main.java
+++ src/finalexplored/dev/lpa/Main.java
@@ -1,6 +1,7 @@
 package finalexplored.dev.lpa;
 
 import consumer.specific.ChildClass;
+import external.util.Logger;
 import finalexplored.dev.lpa.generic.BaseClass;
 
 public class Main {
@@ -21,5 +22,35 @@ public class Main {
         childReferredToAsBase.recommendedStatic();
         System.out.println("--------------");
         child.recommendedStatic();
+
+        System.out.println("--------------");
+        BaseClass.recommendedStatic();
+        ChildClass.recommendedStatic();
+
+        String xArgument = "This is all I've got to say about Section ";
+        StringBuilder zArgument = new StringBuilder("Only saying this: Section ");
+        doXYZ(xArgument, 16, zArgument);
+        System.out.println("After Method, xArgument: " + xArgument);
+        System.out.println("After Method, zArgument: " + zArgument);
+
+        System.out.println("----Logger----");
+        StringBuilder tracker = new StringBuilder("Step 1 is abc");
+        Logger.logToConsole(tracker.toString());
+        tracker.append(", Step 2 is xyz.");
+        Logger.logToConsole(tracker.toString());
+        System.out.println("After logging, tracker = " + tracker);
     }
+
+    private static void doXYZ(String x, int y, final StringBuilder z) {
+        // c는 상수
+        final String c = x + y;
+        System.out.println("c = " + c);
+        x = c;
+
+        // final인 z에 메소드 실행시키는건 괜춘
+        z.append(y);
+        // 새로운 값을 할당하는건 안됨(Cannot assign a value to final variable 'z')
+//        z = new StringBuilder("This is a new reference");
+    }
+
 }
