ada90bbca16a76cbec2d10cd964e5905f2fee5b2
Clara <kimdayoung771@gmail.com>
2024-03-26 20:49:16 +0900

    immutable class

---
+++ src/immutablechallenges/dev/bank/BankAccount.java
@@ -0,0 +1,26 @@
+package immutablechallenges.dev.bank;
+
+public class BankAccount {
+    private final AccountType accountType;
+    private final double balance;
+
+    BankAccount(AccountType accountType, double balance) {
+        this.accountType = accountType;
+        this.balance = balance;
+    }
+
+    public AccountType getAccountType() {
+        return accountType;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    @Override
+    public String toString() {
+        return "%s $%.2f".formatted(accountType, balance);
+    }
+
+    public enum AccountType {CHECKING, SAVINGS}
+}
---
+++ src/immutablechallenges/dev/lpa/Main.java
@@ -0,0 +1,27 @@
+package immutablechallenges.dev.lpa;
+
+import immutablechallenges.dev.bank.BankAccount;
+import immutablechallenges.dev.bank.BankCustomer;
+
+import java.util.List;
+
+
+public class Main {
+    public static void main(String[] args) {
+//        BankAccount account = new BankAccount(BankAccount.AccountType.CHECKING, 500);
+//        System.out.println(account);
+
+        BankCustomer joe = new BankCustomer("Joe", 500.00,
+                10000.00);
+        System.out.println(joe);
+
+        List<BankAccount> accounts = joe.getAccounts();
+        accounts.clear();
+        System.out.println(joe);
+
+//        accounts.add(new BankAccount(BankAccount.AccountType.CHECKING, 150000));
+//        System.out.println(joe);
+    }
+
+
+}
